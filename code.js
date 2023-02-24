47329d6bd3443cb13bc73f7c1e55f2f63fe0166d
Fadi Haddad <fadi.haddad.235@gmail.com>
2023-02-24 22:04:22 +0200

    fixed emoji image, fixed js code for emoji

--- code.js
+++ code.js
@@ -6,11 +6,11 @@ document.getElementById("dice-left").src=`dice-${random_number_1}.png`;
 document.getElementById("dice-right").src=`dice-${random_number_2}.png`;
 if (random_number_1 > random_number_2){
     document.getElementById("header-text").innerText="Player 1 Wins";
-    document.getElementById("emoji-1").style.visibility = "hidden";
+    document.getElementById("emoji-2").style.visibility = "hidden";
 
 }else if(random_number_1 < random_number_2){
     document.getElementById("header-text").innerText="Player 2 Wins";
-    document.getElementById("emoji-2").style.visibility = "hidden";
+    document.getElementById("emoji-1").style.visibility = "hidden";
 }
 else{
     document.getElementById("header-text").innerText="Draw!!";
